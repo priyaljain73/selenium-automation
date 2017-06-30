fc12428cecfe41d6a5c390d8f63d87af5712010f
priyaljain73 <priyaljain73@gmail.com>
2017-06-30 09:42:34 +0530

    Renamed Package

--- RealWorldScenarios/src/com/priyaljain/CalculatorTest.java
+++ RealWorldScenarios/src/com/priyaljain/CalculatorTest.java
@@ -1,4 +1,4 @@
-package com.company;
+package com.priyaljain;
 
 import org.junit.After;
 import org.junit.Assert;
@@ -8,9 +8,6 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.support.ui.WebDriverWait;
-
-import static org.junit.Assert.*;
 
 public class CalculatorTest {
 
