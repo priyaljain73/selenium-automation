28ac33078d6f67c991b5f2e2ffe24a77c8ab02be
priyaljain73 <priyaljain73@gmail.com>
2017-07-01 20:47:34 +0530

    Automates Amazon to Login for a customer and open cart,Add a new item to cart and Remove a specific item from cart

---
+++ RealWorldScenarios/src/com/priyaljain/AmazonTest.java
@@ -0,0 +1,83 @@
+package com.priyaljain;
+
+import org.junit.After;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import static org.junit.Assert.*;
+
+public class AmazonTest {
+
+    //Member Variables
+    WebElement element;
+    WebDriver driver;
+
+    //Member Functions
+
+    @After
+    public void tearDown() throws Exception {
+
+        Thread.sleep(5000);
+        driver.quit();
+    }
+
+    @Test
+    public void testShouldLoginAndOpenCart() throws Exception {
+
+        driver=new FirefoxDriver();
+        driver.get("http://www.amazon.in/");
+        driver.manage().window().maximize();
+        driver.findElement(By.id("nav-link-yourAccount")).click();
+        driver.findElement(By.name("email")).sendKeys("Username");
+        driver.findElement(By.name("password")).sendKeys("Password");
+        driver.findElement(By.id("signInSubmit")).click();
+        driver.findElement(By.id("nav-cart")).click();
+
+    }
+
+    @Test
+    public void testShouldAddItemToCart() throws Exception {
+
+        driver=new HtmlUnitDriver();
+        driver.get("http://www.amazon.in/");
+        driver.findElement(By.id("nav-link-yourAccount")).click();
+        driver.findElement(By.name("email")).sendKeys("Username");
+        driver.findElement(By.name("password")).sendKeys("Password");
+        driver.findElement(By.id("signInSubmit")).click();
+        driver.findElement(By.id("nav-link-shopall")).click();
+        driver.findElement(By.xpath("//a[contains(text(),'All Mobile Phones')]")).click();
+        driver.findElement(By.xpath("//a[contains(text(),'Samsung')]")).click();
+        driver.findElement(By.xpath(".//*[@id='result_6']/div/div[3]/div[1]/a/h2")).click();
+        driver.findElement(By.id("add-to-cart-button")).click();
+        driver.findElement(By.id("nav-cart")).click();
+    }
+
+
+
+    @Test
+    public void testShouldRemoveItemFromCart() throws Exception {
+
+        System.setProperty("webdriver.chrome.driver", "C:\\Appium\\chromedriver_win32\\chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.get("http://www.amazon.in/");
+        driver.manage().window().maximize();
+        driver.findElement(By.id("nav-link-yourAccount")).click();
+        //driver.findElement(By.xpath("./[@id='nav-link-yourAccount']/span[1]")).click();
+        driver.findElement(By.name("email")).sendKeys("Username");
+        driver.findElement(By.name("password")).sendKeys("Password");
+        driver.findElement(By.id("signInSubmit")).click();
+        driver.findElement(By.id("nav-cart")).click();
+        String s=driver.findElement(By.id("nav-cart-count")).getText();
+        if(s!=null)
+            driver.findElement(By.name("submit.delete.C37F534GRPJ869")).click();
+    }
+
+
+}
+
